02a08db5959d6dba71449e98c8d4082b20ea9922
Ben Robbins <ben.robbins@gmail.com>
2020-11-10 12:26:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# brobbot-reddit-image
+
+A brobbot plugin for image searches.
+
+```
+brobbot image <query>
+```
+
+Searches Reddit for `query` and returns 1st image result's URL.
+
+## Configuration
+
+This plugin requires a Reddit app of type 'script' to be configured.
+
+### App ID
+
+```bash
+BROBBOT_REDDIT_IMAGE_APP_ID=ID
+```
+
+Set the app ID used to connect to the Reddit API.
+See https://github.com/reddit-archive/reddit/wiki/OAuth2
+
+### App secret
+
+```bash
+BROBBOT_REDDIT_IMAGE_APP_SECRET=SECRET
+```
+
+Set the app secret used to connect to the Reddit API.
+See https://github.com/reddit-archive/reddit/wiki/OAuth2
+
+### Username
+
+```bash
+BROBBOT_REDDIT_IMAGE_USERNAME=USERNAME
+```
+
+Set the username used to connect to the Reddit API.
+See https://github.com/reddit-archive/reddit/wiki/OAuth2
+
+### Password
+
+```bash
+BROBBOT_REDDIT_IMAGE_PASSWORD=PASSWORD
+```
+
+Set the password used to connect to the Reddit API.
+See https://github.com/reddit-archive/reddit/wiki/OAuth2
+
+### Sort (optional)
+
+```bash
+BROBBOT_REDDIT_IMAGE_SORT=SORT
+```
+
+Set the sort method passed to the Reddit API. E.g. `hot`, `new`, `relevance`. Default is `relevance`.
+
---
+++ index.js
@@ -0,0 +1,45 @@
+const Reddit = require('reddit');
+
+// Description:
+//    Search Reddit posts for images. Uses the 'script' type of reddit application, which requires user credentials
+//
+// Configuration:
+//   BROBBOT_REDDIT_IMAGE_APP_ID - the app ID to pass to reddit
+//   BROBBOT_REDDIT_IMAGE_APP_SECRET - the app secret to pass to reddit
+//   BROBBOT_REDDIT_IMAGE_USERNAME - the username to pass to reddit
+//   BROBBOT_REDDIT_IMAGE_PASSWORD - the user password to pass to reddit
+//   BROBBOT_REDDIT_IMAGE_SORT - the sort type to pass to reddit (e.g. new, hot, relevance). default is 'relevance'
+
+const APP_ID = process.env.BROBBOT_REDDIT_IMAGE_APP_ID;
+const APP_SECRET = process.env.BROBBOT_REDDIT_IMAGE_APP_SECRET;
+const USERNAME = process.env.BROBBOT_REDDIT_IMAGE_USERNAME;
+const PASSWORD = process.env.BROBBOT_REDDIT_IMAGE_PASSWORD;
+const SORT = process.env.BROBBOT_REDDIT_IMAGE_SORT || 'relevance';
+
+module.exports = function(robot) {
+  robot.helpCommand("brobbot image `query`", "Searches Reddit for `query` and returns 1st image result's URL.");
+
+  robot.respond(/^(image|img) (.*)/i, async function(msg) {
+    let url;
+    try {
+      url = await image(msg.match[2]);
+    }
+    catch (err) {
+      console.error(`Reddit image error: ${err}`);
+    }
+    return msg.send(url || 'No result :(');
+  });
+};
+
+async function image(q) {
+  var reddit = new Reddit({
+    username: USERNAME,
+    password: PASSWORD,
+    appId: APP_ID,
+    appSecret: APP_SECRET,
+  });
+
+  const {data} = await reddit.get('/search', {q, limit: 20, type: 'link', sort: SORT});
+  const items = data.children.map(({data: {url, post_hint}}) => ({url, post_hint})).filter(({post_hint}) => post_hint === 'image');
+  return items[0].url;
+}
